3b068e0a9ab09fca27839abb001548876478b667
nekoosuki <1356812370@qq.com>
2021-09-19 15:16:27 +0800

    9-19

--- 122.cpp
+++ 122.cpp
@@ -2,7 +2,11 @@
 #include <vector>
 
 using namespace std;
-
+/*
+只要发现后一天价格比当天低就立刻卖出
+然后买入后一天的
+同时不断刷新买入价格 寻找最小的买入点
+*/
 int maxProfit(vector<int>& prices) {
     int buy = prices[0], profit = 0;
     for (int i = 1; i < prices.size(); i++) {
