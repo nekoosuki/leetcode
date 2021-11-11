cce5b085821b9f2c4c494aa48ce79a8fc37061be
nekoosuki <1356812370@qq.com>
2021-11-11 12:48:18 +0800

    11-11

---
+++ 283.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0,j=0;
+        for(;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+        }
+        while(j!=nums.size()){
+            nums[j++]=0;
+        }
+    }
+};
