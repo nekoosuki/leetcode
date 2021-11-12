30311605f3f41926ea658e671ff7cfdd8389e491
nekoosuki <1356812370@qq.com>
2021-11-12 13:55:44 +0800

    11-22

---
+++ 557.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+class Solution {
+   public:
+    string reverseWords(string s) {
+        int pre_i = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                // reverse(s.begin()+pre_i, s.begin()+i);
+                int p = pre_i, q = i - 1;
+                while (p < q) {
+                    char t = s[p];
+                    s[p] = s[q];
+                    s[q] = t;
+                    p++;
+                    q--;
+                }
+                pre_i = i + 1;
+            }
+        }
+        // reverse(s.begin()+pre_i, s.end());
+        int p = pre_i, q = s.size() - 1;
+        while (p < q) {
+            char t = s[p];
+            s[p] = s[q];
+            s[q] = t;
+            p++;
+            q--;
+        }
+        return s;
+    }
+};
