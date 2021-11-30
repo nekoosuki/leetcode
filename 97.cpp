2c4e10fc4b9fcfc31824ba68c9940854ba83d6bd
nekoosuki <1356812370@qq.com>
2021-11-30 16:45:44 +0800

    11-30

--- 97.cpp
+++ 97.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size(), m = s2.size(), t = s3.size();
-        auto dp = vector<vector<int>>(n+1,vector<int>(m+1,false));
+        auto dp = vector<vector<bool>>(n+1,vector<bool>(m+1,false));
         if (n + m != t) {
             return false;
         }
@@ -14,10 +14,10 @@ public:
             for (int j = 0; j <= m; ++j) {
                 int k = i + j - 1;
                 if (i > 0) {
-                    dp[i][j] |= (dp[i - 1][j] && s1[i - 1] == s3[k]);
+                    dp[i][j] = dp[i][j]||(dp[i - 1][j] && s1[i - 1] == s3[k]);
                 }
                 if (j > 0) {
-                    dp[i][j] |= (dp[i][j - 1] && s2[j - 1] == s3[k]);
+                    dp[i][j] = dp[i][j]||(dp[i][j - 1] && s2[j - 1] == s3[k]);
                 }
             }
         }
